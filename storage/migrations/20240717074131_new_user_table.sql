bb4d28687592361b0591bf1fc3e59ff63ad77da8
wasd <kirshvedov13@gmail.com>
2024-07-19 23:18:38 +0200

    k-8 fix migration

--- storage/migrations/20240717074131_new_user_table.sql
+++ storage/migrations/20240717074131_new_user_table.sql
@@ -3,13 +3,12 @@
 create table if not exists users
 (
     id            uuid primary key default uuid_generate_v4(),
-    username      varchar(50)  not null,
-    password      varchar(510) not null,
+    username      varchar(50) not null unique,
+    password      bytea       not null,
     is_active     boolean          default true,
     date_reg      timestamptz      default now(),
     date_pass_upd timestamptz,
-    constraint password_length_check check ( length(password) > 8 ),
-    constraint username_length_check check ( length(username) > 5 )
+    constraint username_length_check check ( length(username) > 4 )
 );
 -- +goose StatementEnd
 
