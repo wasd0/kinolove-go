66a7bee4573c5085214b51ac08dd43c2e4aeb7d9
wasd <kirshvedov13@gmail.com>
2024-07-19 10:45:37 +0200

    k-8 add migrations script for windows, modify migration script

---
+++ scripts/migrations_run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set GOOSE=%USERPROFILE%\go\bin\goose.exe
+set JET=%USERPROFILE%\go\bin\jet.exe
+set GOOSE_DRIVER=postgres
+set GOOSE_DBSTRING=%DB_URL%
+set GOOSE_MIGRATION_DIR=%MIGRATIONS_PATH%
+
+"%GOOSE%" up
+"%JET%" -dsn="%DB_URL%"?sslmode=disable -schema=public -path=.\.gen
--- scripts/migrations_run.sh
+++ scripts/migrations_run.sh
@@ -3,8 +3,10 @@
 source ".env"
 
 export GOOSE=~/go/bin/goose
+export JET=~/go/bin/jet
 export GOOSE_DRIVER=postgres
 export GOOSE_DBSTRING="$DB_URL"
 export GOOSE_MIGRATION_DIR="$MIGRATIONS_PATH"
 
-exec "$GOOSE" up
+"$GOOSE" up
+exec "$JET" -dsn="${DB_URL}"?sslmode=disable -schema=public -path=./.gen
