e78cf7b764682f1033163f958ff17d4fc90fbdcb
CombiningIdeas <131529697+CombiningIdeas@users.noreply.github.com>
2024-08-05 15:59:15 +0300

    Revert "k-43 добавил две таблицы genres и genres_titles."

--- internal/entity/.gen/kinolove/public/table/table_use_schema.go
+++ internal/entity/.gen/kinolove/public/table/table_use_schema.go
@@ -10,8 +10,6 @@ package table
 // UseSchema sets a new schema name for all generated table SQL builder types. It is recommended to invoke
 // this method only once at the beginning of the program.
 func UseSchema(schema string) {
-	Genres = Genres.FromSchema(schema)
-	GenresTitles = GenresTitles.FromSchema(schema)
 	GooseDbVersion = GooseDbVersion.FromSchema(schema)
 	Movies = Movies.FromSchema(schema)
 	PermissionLevels = PermissionLevels.FromSchema(schema)
